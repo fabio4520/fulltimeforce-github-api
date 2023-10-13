649ec53a923644a167f8930ac184ba1444243427
Fabio Fiestas <fabioleofc@gmail.com>
2023-10-13 09:07:26 -0500

    enable cors for all domains

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { GithubModule } from './github/github.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GithubModule);
+  app.enableCors({ origin: '*' });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
